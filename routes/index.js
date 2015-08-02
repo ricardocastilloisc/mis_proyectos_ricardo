7982fa87d2d3b77ecab7ca552afe329863506801
ricardocastilloisc <ricardocastilloisc@gmail.com>
2015-08-02 14:39:54 -0500

    Autoload DB

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/', function(req, res) {
 });
 
 
+// Autoload de comandos con :quizId
+//Solo se ejecuta en caso de que en los parametros de la ruta exista el parametro 'quizId'
+router.param('quizId',quizController.load);
+
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
